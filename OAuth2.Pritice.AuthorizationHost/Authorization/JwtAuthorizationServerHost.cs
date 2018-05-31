d9a4b8a61daaffa87fa73fd67a2dc2763bfc0e0c
PayneQin <qinyuanpei@chinasofti.com>
2018-05-31 15:26:13 +0800

    Support JWT in OAuth“

---
+++ OAuth2.Pritice.AuthorizationHost/Authorization/JwtAuthorizationServerHost.cs
@@ -0,0 +1,70 @@
+﻿using DotNetOpenAuth.OAuth2;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using DotNetOpenAuth.Messaging.Bindings;
+using DotNetOpenAuth.OAuth2.ChannelElements;
+using DotNetOpenAuth.OAuth2.Messages;
+using Newtonsoft.Json;
+using OAuth2.Pritice.JwtAuthorization;
+
+namespace OAuth2.Pritice.AuthorizationHost.Authorization
+{
+    public class JwtAuthorizationServerHost : IAuthorizationServerHost
+    {
+        public ICryptoKeyStore CryptoKeyStore { get; }
+
+        public INonceStore NonceStore { get; }
+
+        public AutomatedAuthorizationCheckResponse CheckAuthorizeClientCredentialsGrant(IAccessTokenRequest accessRequest)
+        {
+            var userName = accessRequest.ClientIdentifier;
+            var response = new AutomatedUserAuthorizationCheckResponse(accessRequest, true, userName);
+            return response;
+        }
+
+        public AutomatedUserAuthorizationCheckResponse CheckAuthorizeResourceOwnerCredentialGrant(string userName, string password, IAccessTokenRequest accessRequest)
+        {
+            throw new NotImplementedException();
+        }
+
+        public AccessTokenResult CreateAccessToken(IAccessTokenRequest accessTokenRequestMessage)
+        {
+            //var accessToken = new JwtAccessToken();
+            //accessToken.ClientId = accessTokenRequestMessage.ClientIdentifier;
+            //accessToken.Lifetime = TimeSpan.FromMinutes(10);
+
+            //var result = new AccessTokenResult(accessToken);
+            return null;
+        }
+
+        public IClientDescription GetClient(string clientIdentifier)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool IsAuthorizationValid(IAuthorizationDescription authorization)
+        {
+            return true;
+        }
+    }
+
+    public class JwtAccessToken : AccessToken
+    {
+        public string ClientId { get; set; }
+
+        public string ClientSecret { get; set; }
+
+        protected override string Serialize()
+        {
+            return JsonConvert.SerializeObject(new
+            {
+                access_token = JwtTokenManager.GenerateToken(ClientId),
+                token_type = "jwt",
+                expire_in = this.Lifetime
+            });
+        }
+    }
+
+}
---
+++ OAuth2.Pritice.JwtAuthorization/JwtTokenManager.cs
@@ -0,0 +1,91 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OAuth2.Pritice.JwtAuthorization
+{
+    public class JwtTokenManager
+    {
+        /// <summary>
+        /// 对称密钥
+        /// 产生方式：Convert.ToBase64String(new HMACSHA256().Key)
+        /// </summary>
+        private const string secretKey = "db3OIsj+BXE9NZDy0t8W3TcNekrF+2d/1sFnWG4HnV8TZY30iTOdtVWJG8abWvB1GlOgJuQZdcF2Luqm/hccMw==";
+
+        /// <summary>
+        /// 过期时间，默认为30分钟
+        /// </summary>
+        private const int expireMinutes = 30;
+
+        /// <summary>
+        /// 生成令牌
+        /// </summary>
+        /// <param name="clientId">用户名</param>
+        /// <param name="clientSecret">密码(可选)</param>
+        /// <returns></returns>
+        public static string GenerateToken(string clientId, string clientSecret = "")
+        {
+            var symmetricKey = Convert.FromBase64String(secretKey);
+            var tokenHandler = new JwtSecurityTokenHandler();
+
+            var now = DateTime.UtcNow;
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(new[]
+                {
+                    new Claim(ClaimTypes.Name, clientId)
+                }),
+
+                Expires = now.AddMinutes(Convert.ToInt32(expireMinutes)),
+
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(symmetricKey), SecurityAlgorithms.HmacSha256Signature)
+            };
+
+            var stoken = tokenHandler.CreateToken(tokenDescriptor);
+            var token = tokenHandler.WriteToken(stoken);
+
+            return token;
+        }
+
+        /// <summary>
+        /// 验证令牌
+        /// </summary>
+        /// <param name="token">Token</param>
+        /// <returns></returns>
+        public static ClaimsPrincipal ValidateToken(string token)
+        {
+            try
+            {
+                var tokenHandler = new JwtSecurityTokenHandler();
+                var jwtToken = tokenHandler.ReadToken(token) as JwtSecurityToken;
+
+                if (jwtToken == null) return null;
+
+                var symmetricKey = Convert.FromBase64String(secretKey);
+
+                var validationParameters = new TokenValidationParameters()
+                {
+                    RequireExpirationTime = true,
+                    ValidateIssuer = false,
+                    ValidateAudience = false,
+                    IssuerSigningKey = new SymmetricSecurityKey(symmetricKey)
+                };
+
+                SecurityToken securityToken;
+                var principal = tokenHandler.ValidateToken(token, validationParameters, out securityToken);
+
+                return principal;
+            }
+
+            catch (Exception)
+            {
+                return null;
+            }
+        }
+    }
+}
