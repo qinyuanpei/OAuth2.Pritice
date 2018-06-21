789278a10345768c93ffbaba00e04ee3e737c12b
PayneQin <qinyuanpei@chinasofti.com>
2018-06-21 17:42:36 +0800

    I can't help crying

--- OAuth2.Pritice.JwtAuthorization/Models/IdentityModel.cs
+++ OAuth2.Pritice.JwtAuthorization/Models/IdentityModel.cs
@@ -1,9 +1,12 @@
-﻿namespace OAuth2.Pritice.JwtAuthorization.Models
-{
-    using System;
-    using System.Data.Entity;
-    using System.Linq;
+﻿using System;
+using System.Data.Entity;
+using System.Linq;
 
+
+namespace OAuth2.Pritice.JwtAuthorization.Models
+{
+
+    [DbConfigurationType(typeof(MySql.Data.Entity.MySqlEFConfiguration))]
     public class IdentityModel : DbContext
     {
         //您的上下文已配置为从您的应用程序的配置文件(App.config 或 Web.config)
@@ -13,7 +16,7 @@
         //如果您想要针对其他数据库和/或数据库提供程序，请在应用程序配置文件中修改“IdentityModel”
         //连接字符串。
         public IdentityModel()
-            : base("name=IdentityModel")
+            : base("IdentityModel")
         {
         }
 
