b468ce3d9c75fa3949b1a8b727847d04288da0c4
PayneQin <qinyuanpei@chinasofti.com>
2018-06-21 15:10:30 +0800

    Prepare MySQL for Storage

---
+++ OAuth2.Pritice.JwtAuthorization/Models/IdentityModel.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Data.Entity;
+using System.Linq;
+using MySql.Data.Entity;
+
+namespace OAuth2.Pritice.JwtAuthorization.Models
+{
+
+    public class IdentityModel : DbContext
+    {
+        //您的上下文已配置为从您的应用程序的配置文件(App.config 或 Web.config)
+        //使用“IdentityModel”连接字符串。默认情况下，此连接字符串针对您的 LocalDb 实例上的
+        //“OAuth2.Pritice.JwtAuthorization.Models.IdentityModel”数据库。
+        // 
+        //如果您想要针对其他数据库和/或数据库提供程序，请在应用程序配置文件中修改“IdentityModel”
+        //连接字符串。
+        public IdentityModel()
+            : base("name=IdentityModel")
+        {
+
+        }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<IdentityModel>().MapToStoredProcedures();
+        }
+
+        //为您要在模型中包含的每种实体类型都添加 DbSet。有关配置和使用 Code First  模型
+        //的详细信息，请参阅 http://go.microsoft.com/fwlink/?LinkId=390109。
+
+        // public virtual DbSet<MyEntity> MyEntities { get; set; }
+    }
+
+    //public class MyEntity
+    //{
+    //    public int Id { get; set; }
+    //    public string Name { get; set; }
+    //}
+}
